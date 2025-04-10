3ba67f5ae7a3ec770ea761bfba6efde2002b08f1
Lorena <lo.sanchis@gmail.com>
2025-04-10 20:15:09 +0200

    mejora rutas usuario

--- bookly/routes/web.php
+++ bookly/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\LibroController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,7 +13,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+ // Perfil de usuario
+    Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
