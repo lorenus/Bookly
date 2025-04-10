38b51935515816108c6c83cfd3a1f20757f43388
Lorena <lo.sanchis@gmail.com>
2025-04-10 18:45:49 +0200

    migraciones tablas

--- bookly/database/migrations/0001_01_01_000000_create_users_table.php
+++ bookly/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('apellidos');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('password_verified_at')->nullable();
+            $table->string('imgPerfil')->nullable();
+            $table->integer('retoAnual')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ bookly/database/migrations/2025_04_10_160950_create_libros_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->id();
+            $table->string('isbn')->unique;
+            $table->string('titulo');
+            $table->string('autor');
+            $table->integer('numPaginas');
+            $table->string('sinopsis');
+            $table->string('urlPortada')->nullable;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('libros');
+    }
+};
