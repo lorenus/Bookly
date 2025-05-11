50732a912f4d04ff1dcc36cd115665b424e0d3b1
Lorena <lo.sanchis@gmail.com>
2025-05-11 18:09:32 +0200

    etiqueta nombre saludo perfil OK

--- bookly/resources/views/profile/show.blade.php
+++ bookly/resources/views/profile/show.blade.php
@@ -1,42 +1,59 @@
 @extends('layouts.app')
 @section('content')
 <div class="container-fluid">
-    <div class="row g-0"> 
+    <div class="row g-0">
         <!-- Columna Corcho -->
-        <div class="col-md-6 corcho-container">
-            <div class="corcho-content"> <!-- Contenedor para elementos dentro del corcho -->
-                <!-- Foto de perfil -->
-                <div class="profile-section">
-                    <div class="flex-shrink-0">
-                        <!-- <img src="{{ Storage::url($user->imgPerfil) }}?v={{ time() }}"> -->
+        <!-- Columna Corcho -->
+        <div class="col-md-6 corcho-container position-relative"> <!-- Añadido position-relative -->
+            <!-- Div superpuesto en borde superior -->
+            <div class="welcome-banner d-flex flex-column align-items-center justify-content-center">
+                <div style="margin-left: 25%; margin-top: -7%; transform: rotate(-9deg);"> <h2 class="m-0">¡Hola, <br>{{ Auth::user()->name }}!</h2></div>
+            </div>
+
+            <div class="corcho-content h-100 d-flex flex-column pt-5"> <!-- Añadido pt-5 para espacio del banner -->
+                <!-- Primera fila (dividida en 2 columnas) -->
+                <div class="row g-0 mb-3 flex-grow-1">
+                    <!-- Columna izquierda - Polaroid con foto -->
+                    <div class="col-md-6 h-100 pe-2">
+                        <div class="polaroid-frame h-100 d-flex flex-column align-items-center justify-content-center p-4">
+                            <img src="{{ Auth::user()->imgPerfil ? Storage::url(Auth::user()->imgPerfil) : '/img/default-profile.png' }}"
+                                class="polaroid-image"
+                                alt="Foto de perfil">
+                            <div class="polaroid-caption mt-2">Mi perfil</div>
+                        </div>
                     </div>
-                    <div>
-                        <h1 class="text-xl">
-                            Hola, {{ Auth::user()->name }}
-                        </h1>
+
+                    <!-- Columna derecha - Postits con logros -->
+                    <div class="col-md-6 h-100 ps-2">
+                        <div class="postits-container h-100 position-relative p-3">
+                            <div class="postit-large">
+                                <h3>Mis Logros</h3>
+                                <ul class="achievements-list">
+                                    <li>🥇 Primer libro leído</li>
+                                    <li>📚 10 libros completados</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="logros-section">
-                    <h2>Mis logros</h2>
-                    <a href="{{ route('logros.index') }}">
-                </div>
+                <!-- Segunda fila - Papel con reto anual -->
+                <div class="row g-0 flex-grow-1">
+                    <div class="col-12 h-100">
+                        <div class="paper-note h-100 p-4 d-flex flex-column">
+                            <h3 class="text-center mb-3">Reto Anual</h3>
+                            <div class="progress-container mx-auto">
+                                <div class="progress-text text-center mb-2">
+                                    {{ auth()->user()->librosLeidosEsteAnio() }} de {{ auth()->user()->retoAnual }} libros leídos
+                                </div>
 
-                <a href="{{ route('logros.index') }}">Logros</a>
-                <!-- Reto anual -->
-                <div class="challenge-section">
-                    <h2>Reto de Lectura</h2>
-                    <div>
-                        <div class="flex justify-between mb-2">
-                            <span>
-                                {{ auth()->user()->librosLeidosEsteAnio() }} de {{ auth()->user()->retoAnual}} libros
-                            </span>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        
+
         <!-- Columna Libreta -->
         <div class="col-md-6 notebook-container">
             <div class="notebook-content"> <!-- Contenedor para elementos dentro de la libreta -->
