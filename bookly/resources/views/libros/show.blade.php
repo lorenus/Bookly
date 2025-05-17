a32350e444d0bab4c8e9ce886e20e5417d6f7676
Lorena <lo.sanchis@gmail.com>
2025-05-17 13:49:00 +0200

    pagina izquierda ficha libro OK

--- bookly/resources/views/libros/show.blade.php
+++ bookly/resources/views/libros/show.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+<!-- Botón de volver -->
+<a href="{{ route('perfil') }}" class="volver-btn" style="position: fixed;top: 100px;left: 40px;">
+    <img src="{{ asset('img/elementos/volver.png') }}" alt="Volver">
+</a>
+
 <div class="libreta-container">
     <!-- Fondo de libreta -->
     <div class="libreta-background"></div>
@@ -49,15 +54,12 @@
                 </div>
             </div>
 
-            <!-- Separador -->
-            <div class="separator"></div>
-
             <!-- Datos del libro -->
-            <div class="book-details">
+            <div class="book-details mt-3">
                 <h1 class="book-title">{{ $book['volumeInfo']['title'] ?? 'Título desconocido' }}</h1>
 
                 @if(isset($book['volumeInfo']['authors']))
-                <p class="book-author">Autor: {{ implode(', ', $book['volumeInfo']['authors']) }}</p>
+                <p class="book-author">Autor/a: {{ implode(', ', $book['volumeInfo']['authors']) }}</p>
                 @endif
 
                 @if(isset($book['volumeInfo']['publishedDate']))
@@ -68,67 +70,78 @@
                 <p class="book-publisher">Editorial: {{ $book['volumeInfo']['publisher'] }}</p>
                 @endif
             </div>
-
-            <!-- Separador -->
-            <div class="separator"></div>
-
             <!-- Gestión del libro -->
             <div class="book-management">
-                <form action="{{ route('libros.add-to-list') }}" method="POST" class="management-form">
-                    @csrf
-                    <input type="hidden" name="libro_id" value="{{ $book['id'] }}">
-                    <input type="hidden" name="titulo" value="{{ $book['volumeInfo']['title'] ?? '' }}">
-                    <input type="hidden" name="autor" value="{{ implode(', ', $book['volumeInfo']['authors'] ?? []) }}">
-                    <input type="hidden" name="portada" value="{{ $book['volumeInfo']['imageLinks']['thumbnail'] ?? '' }}">
-
-                    <div class="form-group">
-                        <label for="estado">Añadir a lista:</label>
-                        <select name="estado" id="estado" class="form-select" onchange="this.form.submit()">
-                            <option value="">Seleccionar...</option>
-                            <option value="leyendo" {{ Auth::user()->tieneLibroEnLista($book['id'], 'leyendo') ? 'selected' : '' }}>Leyendo</option>
-                            <option value="leido" {{ Auth::user()->tieneLibroEnLista($book['id'], 'leido') ? 'selected' : '' }}>Leído</option>
-                            <option value="porLeer" {{ Auth::user()->tieneLibroEnLista($book['id'], 'porLeer') ? 'selected' : '' }}>Por leer</option>
-                            <option value="favoritos" {{ Auth::user()->tieneLibroEnLista($book['id'], 'favoritos') ? 'selected' : '' }}>Favoritos</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <form action="{{ route('libros.comprar', $book['id']) }}" method="POST">
+                <!-- Primera fila: Añadir a lista + Checkbox -->
+                <div class="row mb-3">
+                    <!-- Columna para Añadir a lista -->
+                    <div class="col-md-6 col-s-12">
+                        <form action="{{ route('libros.add-to-list') }}" method="POST">
                             @csrf
-                            <label class="checkbox-label">
-                                <input type="checkbox" name="comprado"
-                                    {{ Auth::user()->libros()->where('google_id', $book['id'])->wherePivot('comprado', true)->exists() ? 'checked' : '' }}
-                                    onchange="this.form.submit()">
-                                <span>¡Lo tengo!</span>
-                            </label>
+                            <input type="hidden" name="libro_id" value="{{ $book['id'] }}">
+                            <input type="hidden" name="titulo" value="{{ $book['volumeInfo']['title'] ?? '' }}">
+                            <input type="hidden" name="autor" value="{{ implode(', ', $book['volumeInfo']['authors'] ?? []) }}">
+                            <input type="hidden" name="portada" value="{{ $book['volumeInfo']['imageLinks']['thumbnail'] ?? '' }}">
+
+                            <div class="form-group">
+                                <label for="estado">Añadir a lista:</label>
+                                <select name="estado" id="estado" class="form-control" onchange="this.form.submit()">
+                                    <option value="">Seleccionar...</option>
+                                    <option value="leyendo" {{ Auth::user()->tieneLibroEnLista($book['id'], 'leyendo') ? 'selected' : '' }}>Leyendo</option>
+                                    <option value="leido" {{ Auth::user()->tieneLibroEnLista($book['id'], 'leido') ? 'selected' : '' }}>Leído</option>
+                                    <option value="porLeer" {{ Auth::user()->tieneLibroEnLista($book['id'], 'porLeer') ? 'selected' : '' }}>Por leer</option>
+                                    <option value="favoritos" {{ Auth::user()->tieneLibroEnLista($book['id'], 'favoritos') ? 'selected' : '' }}>Favoritos</option>
+                                </select>
+                            </div>
                         </form>
                     </div>
-                </form>
 
-                <div class="action-buttons">
-                    <div class="action-group">
-                        <label>Recomendar a:</label>
-                        <select class="select2-recomendar" style="width: 100%">
-                            <option></option>
-                            @foreach(Auth::user()->amigos as $amigo)
-                            <option value="{{ $amigo->id }}">{{ $amigo->name }}</option>
-                            @endforeach
-                        </select>
+                    <!-- Columna para Checkbox "Lo tengo" -->
+                    <div class="col-md-6 col-s-12 mt-4 d-flex align-items-center">
+                        <form action="{{ route('libros.comprar', $book['id']) }}" method="POST" class="w-100">
+                            @csrf
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="comprado" id="comprado"
+                                    {{ Auth::user()->libros()->where('google_id', $book['id'])->wherePivot('comprado', true)->exists() ? 'checked' : '' }}
+                                    onchange="this.form.submit()">
+                                <label class="form-check-label" for="comprado">
+                                    ¡Lo tengo!
+                                </label>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <!-- Segunda fila: Recomendar + Prestar -->
+                <div class="row">
+                    <!-- Columna para Recomendar -->
+                    <div class="col-md-6 col-sm-6 mb-2">
+                        <div class="form-group">
+                            <label>Recomendar a:</label>
+                            <select class="form-control select2-recomendar">
+                                <option></option>
+                                @foreach(Auth::user()->amigos as $amigo)
+                                <option value="{{ $amigo->id }}">{{ $amigo->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
 
-                    <div class="action-group">
-                        <label>Prestar a:</label>
-                        <select class="select2-prestar" style="width: 100%">
-                            <option></option>
-                            @foreach(Auth::user()->amigos as $amigo)
-                            <option value="{{ $amigo->id }}">{{ $amigo->name }}</option>
-                            @endforeach
-                        </select>
+                    <!-- Columna para Prestar -->
+                    <div class="col-md-6 col-sm-6 mb-2">
+                        <div class="form-group">
+                            <label>Prestar a:</label>
+                            <select class="form-control select2-prestar">
+                                <option></option>
+                                @foreach(Auth::user()->amigos as $amigo)
+                                <option value="{{ $amigo->id }}">{{ $amigo->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
         <!-- Página derecha -->
         <div class="libreta-page right-page">
             <h2 class="sinopsis-title">Sinopsis</h2>
