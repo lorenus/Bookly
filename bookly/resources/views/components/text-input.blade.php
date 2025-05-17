2d10f2c2934524ec4422783ef099b57931827bd4
Lorena <lo.sanchis@gmail.com>
2025-05-17 22:38:52 +0200

    algunes correccions mes

--- bookly/resources/views/components/text-input.blade.php
+++ bookly/resources/views/components/text-input.blade.php
@@ -1,20 +1,20 @@
 @props(['disabled' => false])
 
-<input 
-    @disabled($disabled) 
+<input
+    @disabled($disabled)
     {{ $attributes->merge([
-        'style' => 'display: block; 
+        'style' => 'display: block;
                   font-family: "Providence", sans-serif;
                   color:rgb(0, 0, 0);
-                  width: 100%; 
-                  border: none; 
-                  border-bottom: 2px solid black; 
-                  background-color: transparent; 
-                  border-radius: 0; 
-                  box-shadow: none; 
+                  width: 100%;
+                  border: none;
+                  border-bottom: 2px solid black;
+                  background-color: transparent;
+                  border-radius: 0;
+                  box-shadow: none;
                   text-align: center;
                   outline: none;
-                  padding-bottom: 0; 
+                  padding-bottom: 0;
                   margin-bottom: 5%;
-                  margin-top: 0.1rem;', 
+                  margin-top: 0.1rem;',
     ]) }}>
