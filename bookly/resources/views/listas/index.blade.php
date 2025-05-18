386684adbb5b09e7cee1d5e4cdf780a9ee70216b
Lorena <lo.sanchis@gmail.com>
2025-05-18 15:39:35 +0200

    menu listas responsive OK

--- bookly/resources/views/listas/index.blade.php
+++ bookly/resources/views/listas/index.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+<!-- Botón de volver -->
+<a href="{{ route('perfil') }}" class="position-fixed d-none d-lg-block" style="top: 100px; left: 40px; z-index: 1000;">
+    <img src="{{ asset('img/elementos/volver.png') }}" alt="Volver" width="40" class="volver">
+</a>
 <div class="lists-container relative min-h-screen-misListas overflow-hidden">
     <!-- Fondo decorativo (flores) -->
     <div class="flowers-decoration"></div>
 
-    <!-- Botón de volver -->
-    <a href="{{ route('perfil') }}" class="volver-btn" style="position: fixed;top: 100px;left: 40px;">
-        <img src="{{ asset('img/elementos/volver.png') }}" alt="Volver">
-    </a>
 
     <!-- Contenedor principal -->
     <div class="notebook-wrapper">
