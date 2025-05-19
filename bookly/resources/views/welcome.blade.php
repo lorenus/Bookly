ca8b2d53482d69ebe93efa1a5adb018becd49062
Lorena <lo.sanchis@gmail.com>
2025-05-19 20:22:41 +0200

    home responsive OK

--- bookly/resources/views/welcome.blade.php
+++ bookly/resources/views/welcome.blade.php
@@ -1,138 +1,140 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Bookly</title>
-
+    
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
     <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
 </head>
 
-<body class="position-relative" style="overflow-x: hidden; max-height: 45vh;">
-
-    <div class="background-stack z-1">
-        <div class="background-section">
-            <img src="{{ asset('img/home/fondo.png') }}" alt="Fondo principal" class="bg-image">
-        </div>
-        <div class="background-section">
-            <img src="{{ asset('img/home/estrelles_rosa.png') }}" alt="Estrellas rosas" class="bg-image">
-        </div>
-        <div class="background-section limited-height">
-            <img src="{{ asset('img/home/estrelles_blau.png') }}" alt="Estrellas azules" class="bg-image">
-        </div>
+<body class="d-flex flex-column h-100">
+    <!-- Elementos decorativos fijos -->
+    <div class="position-absolute top-0 start-0 decorativa paper-rosa">
+        <img src="{{ asset('img/home/paper_rosa.png') }}" alt="Decoración" class="img-fluid">
+    </div>
+    <div class="position-absolute top-0 end-0 decorativa estrella-tela">
+        <img src="{{ asset('img/home/estrella_tela.png') }}" alt="Decoración" class="img-fluid">
     </div>
 
-    <div class="position-absolute top-0 start-0 decorativa">
-        <img src="{{ asset('img/home/paper_rosa.png') }}" alt="Decoración" class="img-fluid decorativa-img">
-    </div>
-    <div class="position-absolute top-0 end-0 decorativa">
-        <img src="{{ asset('img/home/estrella_tela.png') }}" alt="Decoración" class="img-fluid decorativa-img">
-    </div>
-
-
-    <div class="container d-flex flex-column justify-content-center align-items-center position-relative">
-
-        <div class="d-flex justify-content-center w-70 mb-4 z-1">
-            <img src="{{ asset('img/elementos/bookly.png') }}" alt="Logo" class="img-fluid">
+    <!-- Contenido principal -->
+    <main class="flex-shrink-0 position-relative">
+        <!-- Fondo principal (detrás del logo y botón) -->
+        <div class="background-wrapper fondo">
+            <img src="{{ asset('img/home/fondo.png') }}" alt="Fondo principal" class="background-image">
         </div>
 
+        <div class="container position-relative z-4 pt-5" style="margin-top: 10%;">
+            <!-- Logo -->
+            <div class="text-center mb-4">
+                <img src="{{ asset('img/elementos/bookly.png') }}" alt="Logo" class="img-fluid">
+            </div>
 
-
-        <div class="d-flex justify-content-center z-1">
-            @if (Auth::check())
-            <a href="{{ route('perfil') }}" class="d-flex justify-content-center">
-                <img src="{{ asset('img/home/boton_entrar.png') }}" alt="Entrar" class="img-fluid">
-            </a>
-            @else
-            <a href="{{ route('login') }}" class="d-flex justify-content-center">
-                <img src="{{ asset('img/home/boton_entrar.png') }}" alt="Entrar" class="img-fluid">
-            </a>
-            @endif
+            <!-- Botón de entrar -->
+            <div class="text-center entrar">
+                @if (Auth::check())
+                    <a href="{{ route('perfil') }}">
+                        <img src="{{ asset('img/home/boton_entrar.png') }}" alt="Entrar" class="img-fluid">
+                    </a>
+                @else
+                    <a href="{{ route('login') }}">
+                        <img src="{{ asset('img/home/boton_entrar.png') }}" alt="Entrar" class="img-fluid">
+                    </a>
+                @endif
+            </div>
         </div>
 
-        <div class="d-flex justify-content-center mt-10 pt-5 z-1 custom-spacing">
-            <img style="width: 70%;" src="{{ asset('img/home/que_es.png') }}" alt="1" class="img-fluid">
+        <!-- Secciones de contenido -->
+        <div class="container position-relative z-4">
+            <div class="text-center my-5 que-es mx-auto" style="width: 70%;">
+                <img src="{{ asset('img/home/que_es.png') }}" alt="¿Qué es Bookly?" class="img-fluid">
+            </div>
+
+            <div class="text-center my-4 mx-auto" style="width: 80%;">
+                <img src="{{ asset('img/home/flecha 1.png') }}" alt="Flecha" class="img-fluid">
+            </div>
+
+            <div class="text-center my-4 mx-auto pegatina" style="width: 80%;">
+                <img src="{{ asset('img/home/pegatina_rosa.png') }}" alt="Pegatina" class="img-fluid">
+            </div>
         </div>
 
-        <div class="d-flex justify-content-center z-1" style="width: 80%;">
-            <img src="{{ asset('img/home/flecha 1.png') }}" alt="flecha" class="img-fluid">
+        <!-- Fondo estrellas rosas (detrás de las siguientes secciones) -->
+        <div class="background-wrapper rosa">
+            <img src="{{ asset('img/home/estrelles_rosa.png') }}" alt="Estrellas rosas" class="background-image">
         </div>
 
-        <div class="d-flex justify-content-center z-1" style="width: 80%;">
-            <img src="{{ asset('img/home/pegatina_rosa.png') }}" alt="pegatina rosa" class="img-fluid">
+        <div class="container position-relative z-4">
+            <div class="text-center my-4 mx-auto" style="width: 80%;">
+                <img src="{{ asset('img/home/flecha 2.png') }}" alt="Flecha" class="img-fluid">
+            </div>
+
+            <div class="text-center my-4 corcho">
+                <img src="{{ asset('img/home/corcho.png') }}" alt="Corcho" class="img-fluid w-100">
+            </div>
         </div>
 
-        <div class="d-flex justify-content-center z-1" style="width: 80%;">
-            <img src="{{ asset('img/home/flecha 2.png') }}" alt="flecha" class="img-fluid">
+        <!-- Fondo estrellas azules (detrás del corcho y footer) -->
+        <div class="background-wrapper blau">
+            <img src="{{ asset('img/home/estrelles_blau.png') }}" alt="Estrellas azules" class="background-image">
         </div>
 
-        <div class="d-flex justify-content-center z-1" style="width: 80%;">
-            <img src="{{ asset('img/home/corcho.png') }}" alt="corcho" class="img-fluid">
-        </div>
+        <div class="container position-relative z-4">
+            <div class="text-center my-4 mx-auto" style="width: 80%;">
+                <img src="{{ asset('img/home/flecha 3.png') }}" alt="Flecha" class="img-fluid">
+            </div>
 
-        <div class="d-flex justify-content-center z-1" style="width: 80%;">
-            <img src="{{ asset('img/home/flecha 3.png') }}" alt="flecha" class="img-fluid">
+            <!-- Contenedor de etiqueta y cohete -->
+            <div class="d-flex justify-content-between align-items-end mt-4 mb-5 position-relative">
+                <div class="etiqueta" style="width: 80%; margin-left: -5%;">
+                    <img src="{{ asset('img/home/etiqueta.png') }}" alt="Etiqueta" class="img-fluid">
+                </div>
+                <div class="cohete-container">
+                    <span class="mb-1 fs-4 d-block text-center">¡Súbeme!</span>
+                    <a href="#">
+                        <img src="{{ asset('img/home/cohete.png') }}" alt="Volver arriba" class="img-fluid">
+                    </a>
+                </div>
+            </div>
         </div>
-
-        <div class="justify-content-center z-1" style="bottom: 20%; margin-left: -10%; margin-top: -5%;">
-            <img src="{{ asset('img/home/etiqueta.png') }}" alt="Etiqueta" class="img-fluid" style="width: 70%;">
-        </div>
-
-        <div class="position-absolute z-1 d-flex flex-column align-items-center" style="right: 2%; bottom: 0;">
-            <span class="mb-1 fs-2">¡Súbeme!</span>
-            <a href="#">
-                <img src="{{ asset('img/home/cohete.png') }}" alt="Volver arriba" class="img-fluid">
-            </a>
-        </div>
-    </div>
+    </main>
 
     <!-- Footer -->
-    <div class="footer-container position-relative mt-5">
-        <!-- Contenedor de la imagen de fondo -->
-        <div class="footer-bg-container">
-            <img src="{{ asset('img/home/fondo_footer.png') }}" alt="Fondo footer" class="footer-bg-img w-100 h-auto">
+    <footer class="mt-auto position-relative z-3">
+        <div class="w-100">
+            <img src="{{ asset('img/home/fondo_footer.png') }}" alt="Fondo footer" class="w-100">
         </div>
-
-        <!-- Contenido del footer superpuesto -->
-        <div class="footer-content position-absolute w-100 h-100 top-0 start-0 d-flex flex-column justify-content-center">
+        
+        <div class="position-absolute top-0 start-0 w-100 h-100 d-flex flex-column justify-content-center">
             <div class="container">
-                <!-- Copyright centrado -->
-                <div class="text-center mb-4">
-                    <p class="text-black m-0">© 2025 Bookly</p>
+                <div class="text-center mb-3">
+                    <p class="m-0">© 2025 Bookly</p>
                 </div>
 
-                <!-- Columnas de enlaces -->
-                <div class="row">
-                    <!-- Columna izquierda -->
-                    <div class="ps-5 col-md-6">
+                <div class="row justify-content-between px-3">
+                    <div class="col-md-6 text-start">
                         <ul class="list-unstyled">
-                            <li><a href="#" class="text-black text-decoration-none">Cookies</a></li>
-                            <li><a href="#" class="text-black text-decoration-none">Términos y condiciones</a></li>
-                            <li><a href="#" class="text-black text-decoration-none">Política de privacidad</a></li>
+                            <li><a href="#" class="text-decoration-none">Cookies</a></li>
+                            <li><a href="#" class="text-decoration-none">Términos y condiciones</a></li>
+                            <li><a href="#" class="text-decoration-none">Política de privacidad</a></li>
                         </ul>
                     </div>
 
-                    <!-- Columna derecha -->
-                    <div class="pe-5 col-md-6">
+                    <div class="col-md-6 text-end">
                         <ul class="list-unstyled">
-                            <li><a href="#" class="text-black text-decoration-none">Instagram</a></li>
-                            <li><a href="#" class="text-black text-decoration-none">Twitter</a></li>
-                            <li><a href="#" class="text-black text-decoration-none">YouTube</a></li>
+                            <li><a href="#" class="text-decoration-none">Instagram</a></li>
+                            <li><a href="#" class="text-decoration-none">Twitter</a></li>
+                            <li><a href="#" class="text-decoration-none">YouTube</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
